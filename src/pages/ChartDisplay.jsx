940b3f3e23cd24a49c8b2ebec88c0488e903737e
kahngjoonk <kk3223@ic.ac.uk>
2025-06-16 18:10:53 +0100

    fixed null errors

--- src/pages/ChartDisplay.jsx
+++ src/pages/ChartDisplay.jsx
@@ -351,6 +351,7 @@ Beta measures a stock's volatility compared to the market`} />:</strong> {riskCa
             <LineChart
               data={visibleStockData}
               onMouseMove={(state) => {
+                if (!state.isTooltipActive || !state.activePayload || !state.activePayload[0]) return null;
                 if (state.isTooltipActive) {
                   const yValue = state.activePayload[0]?.payload.price; // your y key here
                   setHoverY(yValue);
